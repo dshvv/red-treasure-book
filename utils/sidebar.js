30a02c8afe50b411179640f4fe7568d9bf05fa20
丁少华 <960423114@qq.com>
2022-01-27 17:20:48 +0800

    fix: 完成第3章节，开启第三章

--- utils/sidebar.js
+++ utils/sidebar.js
@@ -1,1 +1,1 @@
-module.exports =["index.md","第1章_什么是JavaScript.md","第2章_HTML中的JavaScript.md","第3章_语言基础.md"]
+module.exports =["index.md","第1章_什么是JavaScript.md","第2章_HTML中的JavaScript.md","第3章_语言基础.md","第4章_变量、作用域与内存.md"]

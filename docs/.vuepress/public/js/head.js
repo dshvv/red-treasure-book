e32693e77530ecd165726da302522e842aabe5ae
丁少华 <dingshaohua@aegis-data.cn>
2022-03-03 17:59:02 +0800

    feat: 完成第12章

--- docs/.vuepress/public/js/head.js
+++ docs/.vuepress/public/js/head.js
@@ -6,3 +6,13 @@
 //         console.log(br.previousSibling.innerHTML);
 //     }
 // })
+
+// function Init() {
+//   const el = document.querySelector("#canvas12_1");
+//   if (!el) {  return; }
+//   const ctx = el.getContext("2d");
+//   ctx.fillStyle="#FF0000";
+//   ctx.strokeRect(0, 0, 100, 50); 
+//   ctx.fillText('你好', 10, 10);
+// }
+// setTimeout(Init, 1000);
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,3 @@
 
 # 简介
-![封面](/images/fm.png#w80)
+![封面](/images/fm.png#w50)
